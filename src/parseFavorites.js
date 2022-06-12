fced299c75f61b6f0f7bc60981c9251228caa8bc
potatofish <5317855+potatofish@users.noreply.github.com>
2022-06-11 18:28:03 -0700

    all favourites processed through api with delay

--- src/parseFavorites.js
+++ src/parseFavorites.js
@@ -1,7 +1,12 @@
 const fs = require('fs');
+const axios = require('axios');
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
 const favouritesFile = './data/favorites.txt';
-fs.readFile(favouritesFile, 'utf8', (err, data) => {
+const postURL = 'https://api.e-hentai.org/api.php';
+
+async function processFavourites(err, data) {
     if (err) {
         console.error(err);
         return;
@@ -13,7 +18,13 @@ fs.readFile(favouritesFile, 'utf8', (err, data) => {
         "namespace": 1
     }
 
-    var countAPICalls = 0;
+    //    var startTime = new Date();
+    //    console.log({startTime});
+
+    var completeAPICalls = 0;
+    var entryStartTime = new Date();
+    var now = entryStartTime;
+    var favouritesData = [];
 
     var array = data.split("\r\n")
     for(i in array) {
@@ -25,20 +36,40 @@ fs.readFile(favouritesFile, 'utf8', (err, data) => {
         var gidlistArrayElement = [entryGalleryID, entryGalleryToken]
         entryQueryJSON.gidlist.push([...gidlistArrayElement]);
         if ( entryQueryJSON.gidlist.length == 25) {
-            countAPICalls++;
             //console.log("galleries Queued for Lookup: ", entryQueryJSON.gidlist.length);
-            console.log("POST#%d the following json: ",countAPICalls,  entryQueryJSON);
-
+            
             // TODO Make sure 5 seconds has passed since last API call
-
+            if(completeAPICalls > 0) {
+                now = Date.now();
+                while(now - entryStartTime < 500) {
+                    now = Date.now();
+                }
+                console.log({entryStartTime, now: new Date(now)});
+            }
+            
             // TODO Make POST query call to API
-
-            // TODO Store API Results in a larger data struture for future parsing
-
-            // Clear list of gallery ids for next query
+            //console.log("POST#%d the following json: ",completeAPICalls,  entryQueryJSON);
+            var result = await axios.post(postURL, JSON.stringify(entryQueryJSON))
+            //console.log({data: result.data.gmetadata});
+            var galleryMetaDataResult = result.data.gmetadata;
+            
+            // TODO Parse API Results
+            
+            galleryMetaDataResult.forEach(element => {
+                //console.log({gid: element.gid});
+                favouritesData.push(element);
+                //console.log(key, galleryMetaDataResult[key]);
+              });
+            
+            completeAPICalls++;
             entryQueryJSON.gidlist = [];
+            entryStartTime = new Date();
         }
     }
+    console.log({favouritesData});
 
-    // TODO Parse API Results
-});
+}
+
+(async function Main() {
+    fs.readFile(favouritesFile, 'utf8', processFavourites);
+})();
