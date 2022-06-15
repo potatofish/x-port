12a791844fee8ec3cc76835041f617dccb8a6467
potatofish <5317855+potatofish@users.noreply.github.com>
2022-06-15 10:32:21 -0700

    clean up - better date logic, for each removed

--- src/parseFavorites.js
+++ src/parseFavorites.js
@@ -8,63 +8,45 @@ const { JSDOM } = jsdom;
 const dom = new JSDOM();
 const parser = new dom.window.DOMParser();
 
+// const fsAppendFlag = { flag: 'a+' };
 
 
-
-const delay = ms => new Promise(res => setTimeout(res, ms));
-// const favouritesFile = './data/favorites.txt';
-const favouritesFile = './data/favoritesCopy.txt';
-const favoritesHTML = './results.html';
+const favouritesFile = './data/favorites.txt';
+// const favouritesFile = './data/favoritesCopy.txt';
 const tagSummaryHTML = './tagSummary.html';
 
+const tagMainConfigFile = './resources/pugSource/tagSummaryMain.pug';
+const renderMainTableHTML = pug.compileFile(tagMainConfigFile);
 
-// write a newline to file to clear it
-try {
-    fs.writeFileSync(favoritesHTML, "\n");
-    // file written successfully
-} catch (err) {
-    console.error(err);
-}
-
-var headerStrings = [
-    "<head>", 
-    "<link rel='stylesheet' href='./resources/stylesheets/style.css'>",
-    "</head>",
-    "<body style='font-size:8pt;font-family:arial,helvetica,sans-serif;color:#5C0D11;background:#E3E0D1;padding:2px;margin:0;text-align:center'>",
-    "<div class='main'>",
-    "<div class='footer' style='position: fixed; border:1px solid #5c0d12;; z-index: 999; height: 60px; bottom:0; left:1em; right:1em;display:inline-block'>",
-    "<textarea class='xport_info_textarea'>"
-];
-const fsAppendFlag = { flag: 'a+' };
+const tagTableConfigFile = './resources/pugSource/tagSummaryTable.pug';
+const renderTagTableHTML = pug.compileFile(tagTableConfigFile);
 
 
-headerStrings.forEach(element => {
+const eHentaiAPIURL = 'https://api.e-hentai.org/api.php';
+const eHentaiTagURLPrefix = 'https://e-hentai.org/tag/'
+const eHentaiGalleryURLPrefix = 'https://e-hentai.org/g/';
+
+var apiQueryJSON = {
+    "method": "gdata",
+    "gidlist": [],
+    "namespace": 1
+};
+const apiGidlistMax = 25;
+const apiIntervalMS = 100;
+
+const virtualDocument = parser.parseFromString(renderMainTableHTML(),"text/html");
+
+function logProcessing(aStringToLog) {
     try {
-        fs.writeFileSync(favoritesHTML, element, fsAppendFlag);
-        // console.log({element:element});
+        var lineToWrite = util.inspect(aStringToLog, {showHidden: false, depth: null, colors: false}) +"\n";
+        virtualDocument.getElementById("log").innerHTML += lineToWrite;
+        var serializedString = virtualDocument.childNodes[0].innerHTML
+        fs.writeFileSync(tagSummaryHTML, serializedString);
         // file written successfully
     } catch (err) {
         console.error(err);
     }
-});
-
-/*
-<div class="xport_info_box">
-    <div class="xport_info_header">
-        <div class="xport_info_progress_container1">
-            <div class="xport_info_progress_container2"><div class="xport_info_progress" style="width: 100%;"></div></div></div><div class="xport_info_header_content"><span class="xport_info_header_title">Exported</span><span class="xport_info_header_status xport_info_header_status_hidden">100%</span></div></div><div class="xport_info_content"><div class="xport_info_content_links"><a class="xport_info_content_link">Hide List</a> | <a class="xport_info_content_link">Copy</a> | <a class="xport_info_content_link">Download</a> | <a class="xport_info_content_link">Omit Titles</a> | <span class="xport_info_content_text">Total: 548</span></div></div><div class="xport_info_textarea_container"><div class="xport_info_textarea_line_container"><div class="xport_info_textarea_line"></div></div><textarea class="xport_info_textarea" spellcheck="false" placeholder=""></textarea></div><textarea class="xport_hidden_textarea" style="display: none;"></textarea></div>
-*/
-
-
-
-const tagMainConfigFile = './resources/pugSource/tagSummaryMain.pug';
-const tagTableConfigFile = './resources/pugSource/tagSummaryTable.pug';
-
-const renderMainTableHTML = pug.compileFile(tagMainConfigFile);
-const renderTagTableHTML = pug.compileFile(tagTableConfigFile);
-
-const postURL = 'https://api.e-hentai.org/api.php';
-const tagURLPrefix = 'https://e-hentai.org/tag/'
+}
 
 async function processFavourites(err, data) {
     if (err) {
@@ -72,79 +54,58 @@ async function processFavourites(err, data) {
         return;
     }
 
-    var entryQueryJSON = {
-        "method": "gdata",
-        "gidlist": [],
-        "namespace": 1
-    }
+    var apiCallStartTime = undefined;
+    var galleryMetaDataArray = [];
+    var lines = data.split("\r\n")
 
-    //    var startTime = new Date();
-    // console.log({data});
+    for(i in lines) {
+        var galleryURL = lines[i];
+        var galleryURLSuffix = galleryURL.replace(eHentaiGalleryURLPrefix, '');
 
-    var completeAPICalls = 0;
-    var entryStartTime = new Date();
-    var now = entryStartTime;
-    var favouritesData = [];
-    var processingLog = [];
-
-    const virtualDocument = parser.parseFromString(renderMainTableHTML(),"text/html");
-    function logProcessing(aStringToLog) {
-        try {
-            var lineToWrite = util.inspect(aStringToLog, {showHidden: false, depth: null, colors: false}) +"\n";
-            virtualDocument.getElementById("log").innerHTML += lineToWrite;
-            // var serializer = new XMLSerializer();
-            var serializedString = virtualDocument.childNodes[0].innerHTML
-            console.log({serializedString});
-            fs.writeFileSync(tagSummaryHTML, serializedString);
-            fs.writeFileSync(favoritesHTML, lineToWrite, fsAppendFlag);
-            console.log(lineToWrite);
-            // file written successfully
-        } catch (err) {
-            console.error(err);
+        var gallery = {
+            id: parseInt(galleryURLSuffix.split("/")[0]),
+            token: galleryURLSuffix.split("/")[1]
         }
-    }
+       
+        var entryGalleryID = parseInt(galleryURLSuffix.split("/")[0]);
+        var entryGalleryToken = galleryURLSuffix.split("/")[1];
 
-
-
-    var array = data.split("\r\n")
-    for(i in array) {
-        var entry = array[i];
-        var entryVariables = entry.replace('https://e-hentai.org/g/', '');
-        var entryVariableArray =  entryVariables.split("/");
-        var entryGalleryID = parseInt(entryVariables.split("/")[0]);
-        var entryGalleryToken = entryVariables.split("/")[1];
-        var gidlistArrayElement = [entryGalleryID, entryGalleryToken]
-        entryQueryJSON.gidlist.push([...gidlistArrayElement]);
-        if ( entryQueryJSON.gidlist.length == 25 || i == (array.length - 1)) {
-            logProcessing("galleries Queued for Lookup: " + entryQueryJSON.gidlist.length);
+        var gidlistEntry = [gallery.id, gallery.token]
+        apiQueryJSON.gidlist.push([...gidlistEntry]);
+        
+        // Make API calls when the limit of gallery-ids is met or 
+        // when no lines are live
+        if (apiQueryJSON.gidlist.length == apiGidlistMax || i == (lines.length - 1)) {
             
-            // TODO Make sure 5 seconds has passed since last API call
-            if(completeAPICalls > 0) {
-                now = Date.now();
-                while(now - entryStartTime < 100) {
+            // Ensure interval mseconds have passed since the last API call (if any)
+            // looping until otherwise
+            if (typeof apiCallStartTime === "undefined") {
+                apiCallStartTime = Date.now();
+            }
+            else {
+                var now = Date.now();
+                while(now - apiCallStartTime < apiIntervalMS) {
                     now = Date.now();
                 }
-                logProcessing({entryStartTime, now: new Date(now)});
+                apiCallStartTime = now;
+            }
+            var apiCallStartTimeReadable = new Date(apiCallStartTime)
+            logProcessing(apiQueryJSON.gidlist.length + " Galleries Queued for Lookup @ " + apiCallStartTimeReadable);
+            
+            // Wait for url to return results, then push the contained metadata onto the array
+            // TODO make this async
+            var result = await axios.post(eHentaiAPIURL, JSON.stringify(apiQueryJSON))
+            var galleryMetaDataResult = result.data.gmetadata;
+
+            for (const key in galleryMetaDataResult) {
+                if (Object.hasOwnProperty.call(galleryMetaDataResult, key)) {
+                    const galleryData = galleryMetaDataResult[key];
+                    galleryMetaDataArray.push(galleryData);
+                }
             }
             
-            // TODO Make POST query call to API
-            //console.log("POST#%d the following json: ",completeAPICalls,  entryQueryJSON);
-            var result = await axios.post(postURL, JSON.stringify(entryQueryJSON))
-            //console.log({data: result.data.gmetadata});
-            
-            // Restart the API cooldown timer immediately after returning
-            entryStartTime = new Date(); 
-            var galleryMetaDataResult = result.data.gmetadata;
-            
-            
-            galleryMetaDataResult.forEach(element => {
-                //console.log({gid: element.gid});
-                favouritesData.push(element);
-                //console.log(key, galleryMetaDataResult[key]);
-            });
-            
-            completeAPICalls++;
-            entryQueryJSON.gidlist = [];
+            // clear the list of galleries to look up for next loop
+            apiQueryJSON.gidlist = [];
         }
     }
 
@@ -152,9 +113,9 @@ async function processFavourites(err, data) {
 
     // Build Tag Index Map
     var galleryTags = {};
-    for (const key in favouritesData) {
-        if (Object.hasOwnProperty.call(favouritesData, key)) {
-            const tags = favouritesData[key].tags;
+    for (const key in galleryMetaDataArray) {
+        if (Object.hasOwnProperty.call(galleryMetaDataArray, key)) {
+            const tags = galleryMetaDataArray[key].tags;
             // logProcessing({gallery: favouritesData[key]});
             for (let tagIndex = 0; tagIndex < tags.length; tagIndex++) {
                 var tag = tags[tagIndex];
@@ -181,7 +142,7 @@ async function processFavourites(err, data) {
                     //console.log("NO EXIST");
                 } 
 
-                const DOMparsedDocument = parser.parseFromString(favouritesData[key].title, "text/html");
+                const DOMparsedDocument = parser.parseFromString(galleryMetaDataArray[key].title, "text/html");
                 // const doubleDOMparsedTitle = parser.parseFromString(DOMparsedTitle, "text/html").body.innerHTML;
                 //console.log({tagCount: galleryTags[categoryKey][subCategoryKey]});
                 // var encodedTitle = decodeURIComponent(favouritesData[key].title)+"[ ]";
@@ -199,7 +160,7 @@ async function processFavourites(err, data) {
                 //     console.log(HMTLparsedTitle);
                 // }
                 //console.log(doubleDOMparsedTitle);
-                galleryTags[categoryKey][subCategoryKey].push([favouritesData[key].gid, favouritesData[key].token, HMTLparsedTitle]);
+                galleryTags[categoryKey][subCategoryKey].push([galleryMetaDataArray[key].gid, galleryMetaDataArray[key].token, HMTLparsedTitle]);
 
                 // if (encodedTitle !== favouritesData[key].title ) {
                 //     console.log({encodedTitle, title:favouritesData[key].title});
@@ -243,7 +204,7 @@ async function processFavourites(err, data) {
                         subcategory, 
                         galleries: galleryTags[category][subcategory],
                         size: galleryTags[category][subcategory].length,
-                        url: tagURLPrefix + category.replace(/\s/g, '+') + ":" + subcategory.replace(/\s/g, '+')
+                        url: eHentaiTagURLPrefix + category.replace(/\s/g, '+') + ":" + subcategory.replace(/\s/g, '+')
                     });
                 }
             }
@@ -261,14 +222,14 @@ async function processFavourites(err, data) {
         return 0;
     });
 
-    processingLog.forEach(element => {
-        try {
-            fs.writeFileSync(favoritesHTML, element, fsAppendFlag);
-            // file written successfully
-        } catch (err) {
-            console.error(err);
-        }
-    });
+    // processingLog.forEach(element => {
+    //     try {
+    //         fs.writeFileSync(favoritesHTML, element, fsAppendFlag);
+    //         // file written successfully
+    //     } catch (err) {
+    //         console.error(err);
+    //     }
+    // });
 
     
     // const tagsHTMLTable = summarizeToHTML(tagArray);
@@ -284,38 +245,38 @@ async function processFavourites(err, data) {
 
     // var serializedString = virtualDocument.childNodes[0].innerHTML
 
-    console.log({innerHTML: virtualDocument.getElementById("tag_table").innerHTML});
+    // console.log({innerHTML: virtualDocument.getElementById("tag_table").innerHTML});
     virtualDocument.getElementById("tag_table").innerHTML = encodedtagsHTMLTable;
     var serializedString2 = virtualDocument.childNodes[0].innerHTML
     fs.writeFileSync(tagSummaryHTML, serializedString2);
 
-    console.log({innerHTML: virtualDocument.getElementById("tag_table").innerHTML});
+    // console.log({innerHTML: virtualDocument.getElementById("tag_table").innerHTML});
 
-    var bodyStrings = [
-        // ...processingLog,
-        "</textarea>",
-        "</div>", // closes footer
-        // "<div style='position: fixed; left: 5%; right: 5%; top:0; height: -60px; text-align: center' class='content'>",
-        "<div id='tag_table' class='xport_info_box' style='position: relative; z-index: 99; vertical-align: top; top:0; bottom:60px; left:0; right: 0;display:inline-block'>",
-        //  summarizeToHTML(tagArray),
-        encodedtagsHTMLTable,
-        "</div>",
-        "</div>",
-        "</div>",
-        "</body>"
-    ];
+    // var bodyStrings = [
+    //     // ...processingLog,
+    //     "</textarea>",
+    //     "</div>", // closes footer
+    //     // "<div style='position: fixed; left: 5%; right: 5%; top:0; height: -60px; text-align: center' class='content'>",
+    //     "<div id='tag_table' class='xport_info_box' style='position: relative; z-index: 99; vertical-align: top; top:0; bottom:60px; left:0; right: 0;display:inline-block'>",
+    //     //  summarizeToHTML(tagArray),
+    //     encodedtagsHTMLTable,
+    //     "</div>",
+    //     "</div>",
+    //     "</div>",
+    //     "</body>"
+    // ];
 
-    bodyStrings.forEach(element => {
-        try {
-            fs.writeFileSync(favoritesHTML, element, fsAppendFlag);
-            // console.log({element});
-            // file written successfully
-        } catch (err) {
-            console.error(err);
-        }
-    });
+    // bodyStrings.forEach(element => {
+    //     try {
+    //         fs.writeFileSync(favoritesHTML, element, fsAppendFlag);
+    //         // console.log({element});
+    //         // file written successfully
+    //     } catch (err) {
+    //         console.error(err);
+    //     }
+    // });
 
-    console.log({result: virtualDocument.getElementById("log").innerHTML});
+    // console.log({result: virtualDocument.getElementById("log").innerHTML});
 
     
 
